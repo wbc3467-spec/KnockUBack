24ee6e071860d031cb3f2dc020b42214db32a258
wbc3467-spec <wbc3467@gmail.com>
2026-01-24 12:53:07 +0000

    Feature: allow update players y speed in air

--- src/main/java/net/kairost/knockuback/mixin/PlayerEntityMixin.java
+++ src/main/java/net/kairost/knockuback/mixin/PlayerEntityMixin.java
@@ -43,6 +43,9 @@ public abstract class PlayerEntityMixin extends LivingEntityMixin implements Liv
     private int knockubackCoolDown;
 
     @Unique
+    private boolean onGroundTmp;
+
+    @Unique
     private static final float EPSILON = Float.MIN_VALUE;
 
     @Inject(method = "tick", at = @At("RETURN"))
@@ -92,6 +95,10 @@ public abstract class PlayerEntityMixin extends LivingEntityMixin implements Liv
                 this.lastDamageTakenTmp = this.lastDamageTaken;
                 this.lastDamageTaken = 0;
             }
+            if (ModConfig.INSTANCE.allowAirHit) {
+                this.onGroundTmp = this.isOnGround();
+                this.setOnGround(true);
+            }
             if (!ModConfig.INSTANCE.causeAggro) {
                 this.attackingPlayerTmp = this.attackingPlayer;
                 this.attackingPlayer = null;
@@ -114,6 +121,9 @@ public abstract class PlayerEntityMixin extends LivingEntityMixin implements Liv
                 if (this.knockubackCoolDown == 0) {
                     this.knockubackCoolDown = ModConfig.INSTANCE.comboTick;
                 }
+            }
+            if (ModConfig.INSTANCE.allowAirHit) {
+                this.setOnGround(this.onGroundTmp);
             }
             if (!ModConfig.INSTANCE.causeAggro) {
                 this.playerHitTimer = this.playerHitTimerTmp;
