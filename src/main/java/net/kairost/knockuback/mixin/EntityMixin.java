691dee4ea215b90576dad3b5328c3e706b181199
wbc3467-spec <wbc3467@gmail.com>
2026-01-24 12:53:07 +0000

    Feature: allow update players y speed in air

--- src/main/java/net/kairost/knockuback/mixin/EntityMixin.java
+++ src/main/java/net/kairost/knockuback/mixin/EntityMixin.java
@@ -8,5 +8,11 @@ import org.spongepowered.asm.mixin.Shadow;
 @Mixin(Entity.class)
 public abstract class EntityMixin {
     @Shadow
+    public abstract boolean isOnGround();
+
+    @Shadow
+    public abstract void setOnGround(boolean onGround);
+
+    @Shadow
     public int timeUntilRegen;
 }
