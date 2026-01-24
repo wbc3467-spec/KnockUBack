8c1ea08a0218385ee590631138eba7fbeb87a89c
wbc3467-spec <wbc3467@gmail.com>
2026-01-24 12:53:07 +0000

    Feature: allow update players y speed in air

--- src/main/java/net/kairost/knockuback/command/ModCommands.java
+++ src/main/java/net/kairost/knockuback/command/ModCommands.java
@@ -60,6 +60,24 @@ public class ModCommands {
                         )
                 )
                 .then(
+                    CommandManager.literal("allowAirHit")
+                        .then(
+                            CommandManager.literal("set")
+                                .requires(source -> source.getPermissions().hasPermission(DefaultPermissions.MODERATORS))
+                                .then(
+                                    CommandManager.argument("value", BoolArgumentType.bool())
+                                        .executes(ctx -> {
+                                            boolean value = BoolArgumentType.getBool(ctx, "value");
+                                            return setAirHit(ctx.getSource(), value);
+                                        })
+                                )
+                        )
+                        .then(
+                            CommandManager.literal("get")
+                                .executes(ctx -> getAirHit(ctx.getSource()))
+                        )
+                )
+                .then(
                     CommandManager.literal("comboTick")
                         .then(
                             CommandManager.literal("set")
@@ -153,6 +171,27 @@ public class ModCommands {
 
         source.sendFeedback(
             () -> Text.literal("KnockUBack allow Combo " + value),
+            false
+        );
+        return 1;
+    }
+
+    private static int setAirHit(ServerCommandSource source, boolean value) {
+        ModConfig.INSTANCE.allowAirHit = value;
+        AutoConfig.getConfigHolder(ModConfig.class).save();
+
+        source.sendFeedback(
+            () -> Text.literal("KnockUBack allow Air Hit " + value),
+            true
+        );
+        return 1;
+    }
+
+    private static int getAirHit(ServerCommandSource source) {
+        boolean value = ModConfig.INSTANCE.allowAirHit;
+
+        source.sendFeedback(
+            () -> Text.literal("KnockUBack allow Air Hit " + value),
             false
         );
         return 1;
