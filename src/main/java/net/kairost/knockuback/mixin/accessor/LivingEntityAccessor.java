15c22f2e8bcf6a6edf96583bac987e6a4b3994f3
wbc3467-spec <wbc3467@gmail.com>
2026-01-26 07:50:22 +0000

    Cleanup Feature: Add avoid armor damage option Feature: Add avoid Aggro option

---
+++ src/main/java/net/kairost/knockuback/mixin/accessor/LivingEntityAccessor.java
@@ -0,0 +1,19 @@
+package net.kairost.knockuback.mixin.accessor;
+
+import net.minecraft.entity.LazyEntityReference;
+import net.minecraft.entity.LivingEntity;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(LivingEntity.class)
+public interface LivingEntityAccessor {
+    @Accessor("attackerReference")
+    @Nullable LazyEntityReference<LivingEntity> knockuback$getAttackerReference();
+
+    @Accessor("attackerReference")
+    void knockuback$setAttackerReference(@Nullable LazyEntityReference<LivingEntity> attackerReference);
+
+    @Accessor("lastAttackedTime")
+    void knockuback$setLastAttackedTime(int lastAttackedTime);
+}
