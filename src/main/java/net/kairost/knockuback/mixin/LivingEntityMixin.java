f80a5064465f34d4bda41b186c668b131380e4d1
wbc3467-spec <wbc3467@gmail.com>
2026-01-26 07:50:22 +0000

    Cleanup Feature: Add avoid armor damage option Feature: Add avoid Aggro option

--- src/main/java/net/kairost/knockuback/mixin/LivingEntityMixin.java
+++ src/main/java/net/kairost/knockuback/mixin/LivingEntityMixin.java
@@ -9,12 +9,16 @@ import org.spongepowered.asm.mixin.Shadow;
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends EntityMixin{
     @Shadow
+    public abstract int getLastAttackedTime();
+
+    @Shadow
+    public abstract @Nullable LivingEntity getAttacker();
+
+    @Shadow
     protected int playerHitTimer;
     @Shadow
     @Nullable
     protected PlayerEntity attackingPlayer;
     @Shadow
     protected float lastDamageTaken;
-    @Shadow
-    public int hurtTime;
 }
--- src/main/java/net/kairost/knockuback/mixin/PlayerEntityMixin.java
+++ src/main/java/net/kairost/knockuback/mixin/PlayerEntityMixin.java
@@ -1,6 +1,7 @@
 package net.kairost.knockuback.mixin;
 
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.thrown.EggEntity;
@@ -13,30 +14,56 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.kairost.knockuback.config.ModConfig;
+import net.kairost.knockuback.mixin.accessor.LivingEntityAccessor;
 
 
 @Mixin(PlayerEntity.class)
-public abstract class PlayerEntityMixin extends LivingEntityMixin {
+public abstract class PlayerEntityMixin extends LivingEntityMixin implements LivingEntityAccessor {
     @Unique
     private int timeUntilRegenTmp;
-
-    @Unique
-    private int hurtTimeTmp;
 
     @Unique
     private float lastDamageTakenTmp;
 
     @Unique
+    private PlayerEntity attackingPlayerTmp;
+
+    @Unique
     private int playerHitTimerTmp;
+
+    @Unique
+    private LivingEntity attackerTmp;
+
+    @Unique
+    private int lastAttackedTimeTmp;
 
     @Unique
     private int knockubackCoolDown;
 
     @Unique
-    private PlayerEntity attackingPlayerTmp;
-
-    @Unique
     private static final float EPSILON = Float.MIN_VALUE;
+
+    @Inject(method = "tick", at = @At("RETURN"))
+    private void knockuback$tick(CallbackInfo info) {
+        if (this.knockubackCoolDown > 0) {
+            this.knockubackCoolDown--;
+        }
+    }
+
+    @ModifyVariable(
+        method = "damageArmor",
+        at = @At("HEAD"),
+        argsOnly = true
+    )
+    private float knockuback$modifyDamageArmorAmount(
+        float amount,
+        DamageSource source
+    ) {
+        if (ModConfig.INSTANCE.enabled && !ModConfig.INSTANCE.damageArmor && (source.getSource() instanceof SnowballEntity || source.getSource() instanceof EggEntity) && (source.getAttacker() instanceof PlayerEntity)) {
+            return 0f;
+        }
+        return amount;
+    }
 
     @ModifyVariable(
         method = "damage",
@@ -48,17 +75,10 @@ public abstract class PlayerEntityMixin extends LivingEntityMixin {
         ServerWorld world,
         DamageSource source
     ) {
-        if (ModConfig.INSTANCE.enabled && (source.getSource() instanceof SnowballEntity || source.getSource() instanceof EggEntity) && (source.getAttacker() instanceof PlayerEntity) && amount == 0.0f && this.knockubackCoolDown == 0) {
+        if (ModConfig.INSTANCE.enabled && (source.getSource() instanceof SnowballEntity || source.getSource() instanceof EggEntity) && (source.getAttacker() instanceof PlayerEntity) && this.knockubackCoolDown == 0) {
             amount = EPSILON;
         }
         return amount;
-    }
-
-    @Inject(method = "tick", at = @At("RETURN"))
-    private void knockuback$tick(CallbackInfo info) {
-        if (this.knockubackCoolDown > 0) {
-            this.knockubackCoolDown--;
-        }
     }
 
     @Inject(method = "damage", at = @At("HEAD"))
@@ -67,15 +87,19 @@ public abstract class PlayerEntityMixin extends LivingEntityMixin {
             if (ModConfig.INSTANCE.allowCombo) {
                 this.timeUntilRegenTmp = this.timeUntilRegen;
                 this.timeUntilRegen = 0;
+                this.lastDamageTakenTmp = this.lastDamageTaken;
+                this.lastDamageTaken = 0;
             }
-            this.hurtTimeTmp = this.hurtTime;
-            this.hurtTime = 0;
-            this.lastDamageTakenTmp = this.lastDamageTaken;
-            this.lastDamageTaken = 0;
-            this.playerHitTimerTmp = this.playerHitTimer;
-            this.playerHitTimer = 0;
-            this.attackingPlayerTmp = this.attackingPlayer;
-            this.attackingPlayer = null;
+            if (!ModConfig.INSTANCE.causeAggro) {
+                this.attackingPlayerTmp = this.attackingPlayer;
+                this.attackingPlayer = null;
+                this.playerHitTimerTmp = this.playerHitTimer;
+                this.playerHitTimer = 0;
+                this.attackerTmp = this.getAttacker();
+                knockuback$setAttacker(null);
+                this.lastAttackedTimeTmp = this.getLastAttackedTime();
+                knockuback$setLastAttackedTime(0);
+            }
         }
     }
 
@@ -84,15 +108,17 @@ public abstract class PlayerEntityMixin extends LivingEntityMixin {
         if (ModConfig.INSTANCE.enabled && (source.getSource() instanceof SnowballEntity || source.getSource() instanceof EggEntity) && (source.getAttacker() instanceof PlayerEntity)) {
             if (ModConfig.INSTANCE.allowCombo) {
                 this.timeUntilRegen = timeUntilRegenTmp;
+                this.lastDamageTaken = this.lastDamageTakenTmp;
                 if (this.knockubackCoolDown == 0) {
                     this.knockubackCoolDown = ModConfig.INSTANCE.comboTick;
                 }
             }
-
-            this.hurtTime = this.hurtTimeTmp;
-            this.lastDamageTaken = this.lastDamageTakenTmp;
-            this.playerHitTimer = this.playerHitTimerTmp;
-            this.attackingPlayer = this.attackingPlayerTmp;
+            if (!ModConfig.INSTANCE.causeAggro) {
+                this.playerHitTimer = this.playerHitTimerTmp;
+                this.attackingPlayer = this.attackingPlayerTmp;
+                knockuback$setAttacker(this.attackerTmp);
+                knockuback$setLastAttackedTime(this.lastAttackedTimeTmp);
+            }
         }
     }
 }
