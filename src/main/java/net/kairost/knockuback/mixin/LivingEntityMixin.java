719bc50aa3975aa791d98f97beae70041ba79dfc
wbc3467-spec <wbc3467@gmail.com>
2026-01-26 07:50:22 +0000

    Cleanup Feature: Add avoid armor damage option Feature: Add avoid Aggro option

--- src/main/java/net/kairost/knockuback/mixin/LivingEntityMixin.java
+++ src/main/java/net/kairost/knockuback/mixin/LivingEntityMixin.java
@@ -10,12 +10,13 @@ import org.spongepowered.asm.mixin.Shadow;
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends EntityMixin{
     @Shadow
+    public abstract int getLastAttackedTime();
+
+    @Shadow
     @Nullable
     protected LazyEntityReference<PlayerEntity> attackingPlayer;
     @Shadow
     protected int playerHitTimer;
     @Shadow
     protected float lastDamageTaken;
-    @Shadow
-    public int hurtTime;
 }
